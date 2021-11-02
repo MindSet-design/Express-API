dd083ffdc69a549c2fa4662df0261fd72ead33d3
MindSet-design <nicnow007@gmail.com>
2021-11-02 13:27:34 -0600

    using dot env now

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 const express = require ('express');
+const dotenv=require('dotenv');
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -8,7 +10,7 @@ app.use(express.urlencoded());
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 5000;
 
-MongoClient.connect('mongodb+srv://Mongo_Nick:Vxxbgl8xP2758Tyg@servercluster.edziw.mongodb.net/survey_results?retryWrites=true&w=majority',
+MongoClient.connect(uri,
  { useUnifiedTopology:true})
     .then(client=>{
         console.log('connected to database')
